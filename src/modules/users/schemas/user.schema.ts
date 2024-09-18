49740a0de75e671bdf40ee64fc9bb200a77c08a5
dennis <dennis.nguyen0909@gmail.com>
2024-09-18 20:46:18 +0700

    setup project

---
+++ src/modules/users/schemas/user.schema.ts
@@ -0,0 +1,41 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { HydratedDocument } from 'mongoose';
+
+export type UserDocument = HydratedDocument<User>;
+@Schema({ timestamps: true })
+export class User {
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: number;
+
+  @Prop()
+  password: string;
+
+  @Prop()
+  phone: string;
+
+  @Prop()
+  address: string;
+
+  @Prop()
+  image: string;
+
+  @Prop()
+  role: string;
+
+  @Prop()
+  accountType: string;
+
+  @Prop()
+  isActive: string;
+
+  @Prop()
+  codeId: string;
+
+  @Prop()
+  codeExpired: string;
+}
+
+export const UserSchema = SchemaFactory.createForClass(User);
